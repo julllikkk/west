bad55ec993440dcbd465a96ce56039d5df54f70c
Tatiana Gugina <Tatiana.Gugina@urfu.me>
2025-03-18 16:58:40 +0500

    fix

--- src/index.js
+++ src/index.js
@@ -62,69 +62,6 @@ class Dog extends Creature {
     }
 }
 
-import Card from './Card.js';
-import Game from './Game.js';
-import TaskQueue from './TaskQueue.js';
-import SpeedRate from './SpeedRate.js';
-
-class Creature extends Card {
-    constructor(name, maxPower, image) {
-        super(name, maxPower, image);
-    }
-
-    getDescriptions() {
-        return [getCreatureDescription(this), super.getDescriptions()]
-    }
-}
-
-
-// Отвечает является ли карта уткой.
-function isDuck(card) {
-    return card && card.quacks && card.swims;
-}
-
-// Отвечает является ли карта собакой.
-function isDog(card) {
-    return card instanceof Dog;
-}
-
-// Дает описание существа по схожести с утками и собаками
-function getCreatureDescription(card) {
-    if (isDuck(card) && isDog(card)) {
-        return 'Утка-Собака';
-    }
-    if (isDuck(card)) {
-        return 'Утка';
-    }
-    if (isDog(card)) {
-        return 'Собака';
-    }
-    return 'Существо';
-}
-
-
-
-// Основа для утки.
-class Duck extends Creature {
-    constructor(name = 'Мирный житель', power = 2, image) {
-        super(name, power, image);
-    }
-
-    quacks = function () {
-        console.log('quack')
-    };
-    swims = function () {
-        console.log('float: both;')
-    };
-}
-
-
-// Основа для собаки.
-class Dog extends Creature {
-    constructor(name = "Бандит", maxPower = 3, image) {
-        super(name, maxPower, image);
-    }
-}
 
 class Gatling extends Creature {
     constructor(name = 'Гатлинг', power = 6, image) {
@@ -173,53 +110,12 @@ const seriffStartDeck = [
     new Duck(),
     new Duck(),
     new Duck(),
+    new Gatling(),
 ];
-
-// Колода Бандита, верхнего игрока.
 const banditStartDeck = [
     new Trasher(),
-];
-
-
-// Создание игры.
-const game = new Game(seriffStartDeck, banditStartDeck);
-
-// Глобальный объект, позволяющий управлять скоростью всех анимаций.
-SpeedRate.set(1);
-
-// Запуск игры.
-game.play(false, (winner) => {
-    alert('Победил ' + winner.name);
-});
-
-
-class Trasher extends Dog {
-    constructor(name = "Громила", maxPower = 5, ...args) {
-        super(name, maxPower, ...args);
-    }
-
-    modifyTakenDamage(value, fromCard, gameContext, continuation) {
-        this.view.signalAbility(() => {
-            continuation(value - 1)
-        });
-    }
-
-    getDescriptions() {
-        return ['Получает на 1 меньше урона', super.getDescriptions()];
-    }
-}
-
-
-// Колода Шерифа, нижнего игрока.
-const seriffStartDeck = [
-    new Duck(),
-    new Duck(),
-    new Duck(),
-];
-
-// Колода Бандита, верхнего игрока.
-const banditStartDeck = [
-    new Trasher(),
+    new Dog(),
+    new Dog(),
 ];
 
 
